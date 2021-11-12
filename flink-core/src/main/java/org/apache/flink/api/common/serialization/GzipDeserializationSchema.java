5d0c9ce8f8afa0ca65972824b279bcab8abf57ef
Francis Conroy <francis.conroy@switchdin.com>
2021-11-12 11:11:54 +1100

    Handle any amount of bytes!

--- flink-core/src/main/java/org/apache/flink/api/common/serialization/GzipDeserializationSchema.java
+++ flink-core/src/main/java/org/apache/flink/api/common/serialization/GzipDeserializationSchema.java
@@ -6,13 +6,17 @@ import java.util.zip.*;
 public class GzipDeserializationSchema extends AbstractDeserializationSchema<byte[]> {
     @Override
     public byte[] deserialize(byte[] message) throws IOException {
-        ByteArrayInputStream compressedStream = new ByteArrayInputStream(message);
-        GZIPInputStream zipStream = new GZIPInputStream(compressedStream, message.length);
-        byte[] decompressedData = new byte[message.length];
-        zipStream.read(decompressedData);
-        zipStream.close();
-        compressedStream.close();
-        return decompressedData;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ByteArrayInputStream bis = new ByteArrayInputStream(message);
+        GZIPInputStream in = new GZIPInputStream(bis);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = in.read(buffer)) >= 0) {
+            bos.write(buffer, 0, len);
+        }
+        in.close();
+        bos.close();
+        return bos.toByteArray();
     }
 }
 
