53e7fbc10711f0795a0642123474adc26fbf8905
Francis Conroy <francis.conroy@switchdin.com>
2021-11-16 13:56:16 +1100

    Add a switchdin RMQ deserialization schema so we can get the topic and the raw payload out of the RMQ message

---
+++ flink-connectors/flink-connector-rabbitmq/src/main/java/org/apache/flink/streaming/connectors/rabbitmq/RMQDeserializationSchemaSwitchdin.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.connectors.rabbitmq;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.serialization.RawDeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+/**
+ * A {@link RMQDeserializationSchema} implementation that uses the {@link
+ * RawDeserializationSchema} to return the routing key and the raw bytes from the message.
+ */
+final class RMQDeserializationSchemaSwitchdin implements RMQDeserializationSchema<Tuple2<String, byte[]>> {
+    private final TypeInformation<Tuple2<String, byte[]>> type;
+    private DeserializationSchema<byte[]> schema;
+
+    public RMQDeserializationSchemaSwitchdin() {
+        schema = new RawDeserializationSchema();
+        this.type = TypeExtractor.createTypeInfo(
+                RMQDeserializationSchemaSwitchdin.class, getClass(), 0, null, null);
+    }
+
+    @Override
+    public void deserialize(
+            Envelope envelope,
+            AMQP.BasicProperties properties,
+            byte[] body,
+            RMQCollector<Tuple2<String, byte[]>> collector)
+            throws IOException {
+        collector.collect(new Tuple2(envelope.getRoutingKey(), schema.deserialize(body)));
+    }
+
+    @Override
+    public TypeInformation<Tuple2<String, byte[]>> getProducedType() {
+        return this.type;
+    }
+
+    @Override
+    public void open(DeserializationSchema.InitializationContext context) throws Exception {
+        schema.open(context);
+    }
+
+    @Override
+    public boolean isEndOfStream(Tuple2<String, byte[]> nextElement) {
+        return false;
+    }
+}
